82da6a8f2aa672e2d5a310d5a67a3e275a88fd0a
Fatemeh <mohammadi.f8687@gmail.com>
2025-06-05 06:40:55 -0700

    added insta account and post models to models.py and also logged in on insta

--- app.py
+++ app.py
@@ -3,28 +3,16 @@ from config import (app, db, Resource, api, request, session, jsonify, JWTManage
 from models import Users, generate_password_hash, check_password_hash
 from functools import wraps
 from flask_jwt_extended import get_jwt, set_access_cookies, unset_jwt_cookies
+from instagrapi import Client
+import PIL
 
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
-    """
-
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        current_user = get_jwt_identity()
-        if not current_user:
-            return {"message": "Unutherized"}, 404
-        return f(*args, **kwargs)
-
-    return decorated_function
 
 
 class UserRegister(Resource):
     '''
     register the users
     '''
+
     def post(self):
         data = request.get_json()
 
@@ -40,7 +28,6 @@ class UserRegister(Resource):
 
         if 'confirm' not in data:
             return {"message": "your password confirmation is missing"}, 400
-
 
         # check if the fields are empty
         if not data['user_name'] or data['user_name'] == "":
@@ -74,6 +61,7 @@ class UserLogin(Resource):
     '''
     login the user
     '''
+
     def post(self):
         data = request.get_json()
 
@@ -96,10 +84,10 @@ class UserLogin(Resource):
         access_token = create_access_token(identity=user.user_name)
 
         response = make_response({
-                "message": "you are logged in",
-                "access token": access_token,
-                "user_id": user.id
-            }, 200)
+            "message": "you are logged in",
+            "access token": access_token,
+            "user_id": user.id
+        }, 200)
 
         # Set cookies if needed
         response.set_cookie(
@@ -111,18 +99,68 @@ class UserLogin(Resource):
 
         return response
 
+class InstaLogin(Resource):
+    '''
+    connect to instagram account
+    '''
+    @jwt_required()
+    def post(self):
+        # ask the user to input their instagtam info
+        data = request.get_json()
+
+        if 'username_insta' not in data or 'password_insta' not in data:
+            return {"message": "you have missed username or password"}, 400
+
+        if not data['username_insta'] or data['username_insta'] == "":
+            return {"message": "please enter your instagram username"}, 400
+
+        if not data['password_insta'] or data['password_insta'] == "":
+            return {"message": "please enter your instagram password"}
+
+        try:
+            current_user = get_jwt_identity()
+            # query which account belong to the user
+            exist_user_account = Insta_info.query.filter_by(
+                username_insta = data['username_insta'],
+                user_id = current_user
+            ).first()
+
+            if exist_user_account:
+                return {"message": "user already exists"}, 400
+
+            # make instance of client
+            user = Client()
+
+            # login the user to their instagram account
+            user.login(data['username_insta'], data['password_insta'])
+
+            new_insta_user = Insta_info(
+                username_insta=data['username_insta'],
+                password_insta=data['password_insta'],
+                user_id = current_user
+            )
+
+            db.session.add(new_insta_user)
+            db.session.commit()
+
+            return {"message": "user connected"}, 200
+
+        except Exception as e:
+            return {"message": "not successful"}, 400
 
 
 class UserLogout(Resource):
     '''
     logout route
     '''
+
     @jwt_required()
     def post(self):
         response = make_response({"message": "you are logged out"}, 200)
 
         unset_jwt_cookies(response)
         return response
+
 
 # api routes
 api.add_resource(UserRegister, '/register')
--- models.py
+++ models.py
@@ -5,9 +5,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Users(db.Model):
+    __tablename__ = 'Users'
+
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+    insta_user_account = db.relationship("Insta_info", back_populates="insta_account", lazy="dynamic")
+
+
+
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -18,5 +25,49 @@ class Users(db.Model):
     def to_json(self):
         return {
             "id": self.id,
-            "user_name": self.user_name
+            "user_name": self.user_name,
+        }
+
+class Insta_info(db.Model):
+    __tablename__ = 'Insta_info'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username_insta = db.Column(db.String(120), unique=True, nullable=False)
+    password_insta = db.Column(db.String(120), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.id"), nullable=False, unique=True)
+    user_service = db.relationship("Users", back_populates="insta_user_account")
+
+    post = db.relationship("Post_insta", back_populates="instagram_posts", uselist=False)
+
+
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "user_service_id": self.user_id,
+            "username_insta": self.username_insta,
+        }
+
+
+class Post_insta(db.Model):
+    __tablename__ = 'Post_insta'
+
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(300), nullable=False)
+    caption = db.Column(db.String(600), nullable=True, default="")
+
+    user_id_account_instagram = db.Column(db.Integer, db.ForeignKey("Insta_info.id"), nullable=False, unique=True)
+    connect_to_account_insta = db.relationship("Insta_info", back_populates="post")
+
+
+
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "insta_id": self.insta_id,
+            "user_id": self.user_id,
+            "caption": self.caption,
+            "path": self.path,
         }
